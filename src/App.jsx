f73be7781d3c61ba154dd2349d6b1516fd01100b
sheilabett <sheilahbett@gmail.com>
2023-11-07 22:25:26 +0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import './App.css'
-
+import './index.css'
+import {Route,Routes, BrowserRouter} from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom';
 import ArticleList from './Components/ArticleList'
-import AddArticle from './Components/AddArticle';
 import Home from './Components/Home';
 import NavBar from './Components/NavBar';
+import Comments from './Components/Comments';
+
 
 function App () {
     return (
             <div className="App">
-             <NavBar/>
-              <Switch>
-                <Route exact path="/">
-                <Home/>
-                </Route>
-                <Route path="/articlelist">
-                <ArticleList />
-                </Route>
-                {/* <Route  path ="/comments">
-                <Comments/>
-                </Route> */}
-              </Switch>
+                <Router>
+                    <NavBar/>
+                <Routes>
+                <Route exact path="/" Component={Home}></Route>     
+                <Route path="/articlelist" Component={ArticleList}></Route>
+                <Route path="/comments" Component={Comments}></Route> 
+                </Routes>
+                </Router>
             </div>
     );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import {NavLink} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom"; // Import the Link component from React Router
 
 function NavBar() {
-
-  const linkStyles = {
-    textAlign: "center",
-    marginLeft:"10px",
-    textDecoration: "none",
-    color: "black",
-    fontWeight: "bold",
-    fontSize: "30px",
-    padding: "10px 20px",
-  }
-
-  return (
-    <nav>
-      <NavLink to="/" style={linkStyles}>Home</NavLink>
-      <NavLink to="/articlelist" style={linkStyles} >Articles</NavLink>
-      <NavLink to="/comments" style={linkStyles} >Comments</NavLink>
-
-    </nav>
-  )
+    const linkStyles = {
+        textAlign: "center",
+        marginLeft:"10px",
+        textDecoration: "none",
+        color: "black",
+        fontWeight: "bold",
+        fontSize: "30px",
+        padding: "10px 20px",
+      }
+    return (
+        <nav>
+            <Link to="/" style={linkStyles}>Home</Link>
+            <Link to="/articlelist" style={linkStyles} >Articles</Link>
+            <Link to="/comments" style={linkStyles} >Comments</Link>
+            {/* Add other links here using the Link component */}
+        </nav>
+    );
 }
+
 export default NavBar;
