dd762427a1b205019851a2d20826958983f08fc3
sheilabett <sheilahbett@gmail.com>
2023-11-07 17:32:13 +0300

    Merged conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './App.css'
-//import Route from  "reat-router-dom"
+
 import ArticleList from './Components/ArticleList'
+import AddArticle from './Components/AddArticle';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
 
 function App () {
     return (
-        <div>
-        <Articlelist/>
-        
-        </div>
+            <div className="App">
+             <NavBar/>
+              <Switch>
+                <Route exact path="/">
+                <Home/>
+                </Route>
+                <Route path="/articlelist">
+                <ArticleList />
+                </Route>
+                {/* <Route  path ="/comments">
+                <Comments/>
+                </Route> */}
+              </Switch>
+            </div>
     );
 }
 export default App;
