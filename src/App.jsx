e71613f25e531bb689854b175ca8f30f098727aa
YOUR NAME <YOUR@EMAIL.com>
2023-11-07 16:00:37 +0300

    App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import './App.css'
-//import Route from  "reat-router-dom"
-import ArticleList from './Components/ArticleList'
+import './App.css';
+import React from 'react';
+//import {Route, Switch} from "react-router-dom"
+import ArticleList from './Components/ArticleList';
+import Navbar from './Components/NavBar';
 
-function App () {
-    return (
-        <div>
-        <Articlelist/>
-        
-        </div>
-    );
+
+
+function App() {
+  return (
+    
+   
+    <div className="App">
+    //  <NavBar/>
+    //   <Switch>
+    //     <Route exact path="/">
+    //     <Home/>
+    //     </Route>
+    //     <Route path="/articlelist">
+    //     <ArticleList />
+    //     </Route>
+    //     <Route  path ="/comments">
+    //     <Comments/>
+    //     </Route>
+    //   </Switch>
+    </div>
+  );
 }
+
 export default App;
