62d408ec870d1bbbfebb01e9a1e68868c3e99b7d
morrison <morrisonwanja@gmail.com>
2023-11-07 11:57:54 +0300

    Project in progress

--- src/Components/AddArticle.jsx
+++ src/Components/AddArticle.jsx
@@ -5,6 +5,7 @@ import "./Style.css";
 function AddArticle(){
     const [name, setName] = useState("")
     const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
     const [description, setDescription] = useState("")
     const [content, setContent] = useState("")
     const [author, setAuthor] = useState("")
@@ -41,22 +42,26 @@ function AddArticle(){
 
     return(
         <>
-        <form className="new-article" onSubmit={handleSubmit}>
+        <form className="new-article" onSubmit={handleSubmit} autoComplete="off">
         <input type="text" name="name" placeholder="Name of your article" 
         value={name}
-        onChange={(e) => setName(e.target.value)}/>
+        onChange={(e) => setName(e.target.value)} autoComplete="off"/>
         <input type="text" name="title" placeholder="Title" 
         value={title}
-        onChange={(e) => setTitle(e.target.value)}/>
-         <img src="" name="image" alt="AddImage link here" 
+        onChange={(e) => setTitle(e.target.value)} autoComplete="off"/>
+         <input name="image" placeholder="AddImage link here" 
          value={image}
-         onChange={(e) => setImage(e.target.value)}/>
+         onChange={(e) => setImage(e.target.value)} autoComplete="off"/>
          <input type="text" name="description" placeholder="Description" 
          value={description}
          onChange={(e) => setDescription(e.target.value)}
-         />
-         <input type="text" placeholder="Content" />
-         <input type="text" placeholder="Author" />
+         autoComplete="off"/>
+         <input type="text" name="content" placeholder="Content"
+         value={content}
+         onChange={(e) => setContent(e.target.value)} autoComplete="off" />
+         <input type="text" name="author" placeholder="Author" 
+         value={author}
+         onChange={(e) => setAuthor(e.target.value)} autoComplete="off" />
          <input type="submit" value="Add Article" />
         </form>
         <p>Welcome to my section</p>
