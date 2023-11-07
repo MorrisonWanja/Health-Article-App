c6ff2a69519f71b223b7bb808bc147cdbf7f53bb
JOY254 <nyorgijoy@gmail.com>
2023-11-06 23:33:40 -0600

    added LinkStyles properties

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,5 +2,13 @@ import React from "react"
 import {NavLink} from "react-router-dom"
 
 function Navbar () {
-    
+
+    const LinkStyles = {
+        color:"green",
+        textAlign:"centre",
+        fontWeight:"bold",
+        fontsize:"26px",
+        padding:"15px",
+        marginleft:"12px", 
+    }
 }
