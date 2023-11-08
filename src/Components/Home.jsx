2e6c8e3fa82b33a303b02aa1372d93dac55b66d4
morrisonwanja@gmail.com <morrison@DESKTOP-IK00ASL.localdomain>
2023-11-08 18:25:22 +0300

    Project in progress

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,12 +5,14 @@ import UpdaeArticle from './UpdateArticle'
 function Home() {
   return (
     <div className="home-div" >
+      <UpdaeArticle />
       <div className= "home">
       <h1 className="home-head" >HEALTH ARTICLES APP</h1>
         <p className="home-par" >Welcome to our health articles app. 
            Here you can get information about health. <br></br>
            They are written by very competitive authors and are very interesting.
            Enjoy reading the articles.</p>
+           
       </div>
     </div>
   )
--- src/Components/UpdateArticle.jsx
+++ src/Components/UpdateArticle.jsx
@@ -2,10 +2,45 @@
 import React,{useState} from "react";
 
 function UpdaeArticle(){
+    const[newName,setName] = useState("");
+    const[newImage,setImage] = useState("");
+    const[newTitle,setTitle] = useState("");
+    const[newDescription,setDescription] = useState("");
+    const[newContent,setContent] = useState("");
+    const[newAuthor,setAuthor] = useState("");
 
     return(
     <div id="modal">
       <h1>Welcome Home</h1>
+      <form >
+        <input type="text" placeholder="New name"  
+        value={newName} 
+        onChange={(e) => setName(e.target.value)}
+         />
+         <input type="text" placeholder="Add Image address here"  
+        value={newImage} 
+        onChange={(e) => setImage(e.target.value)}
+         />
+         <input type="text" placeholder="New Title"  
+        value={newTitle} 
+        onChange={(e) => setTitle(e.target.value)}
+         />
+         <input type="text" placeholder="New Description"  
+        value={newDescription} 
+        onChange={(e) => setDescription(e.target.value)}
+         />
+         <input type="text" placeholder="Update Content"  
+        value={newContent} 
+        onChange={(e) => setContent(e.target.value)}
+         />
+         <input type="text" placeholder="New name"  
+        value={newAuthor} 
+        onChange={(e) => setAuthor(e.target.value)}
+         />
+         <input type="submit" placeholder="New name"  
+        value="Update Article"
+         />
+      </form>
     </div>
 
     )
