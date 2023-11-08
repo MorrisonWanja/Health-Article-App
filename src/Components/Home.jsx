4da4dad7e04f06a8054e459cecb62e04a7545dcc
morrisonwanja@gmail.com <morrison@DESKTOP-IK00ASL.localdomain>
2023-11-08 17:55:46 +0300

    Project in progress

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import UpdaeArticle from './UpdateArticle'
 
 
 function Home() {
---
+++ src/Components/UpdateArticle.jsx
@@ -0,0 +1,16 @@
+
+import React,{useState} from "react";
+
+function UpdaeArticle(){
+
+    return(
+    <div id="modal">
+      <h1>Welcome Home</h1>
+    </div>
+
+    )
+
+
+}
+
+export default UpdaeArticle;
