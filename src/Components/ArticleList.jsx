2f7d0e7d75bf5497a31a19ae4f7f36a2582dff57
morrisonwanja@gmail.com <morrison@DESKTOP-IK00ASL.localdomain>
2023-11-24 12:57:44 +0300

    Complete Project

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -6,64 +6,85 @@ function ArticleList() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchedArticles = () => {
-      //  fetch("http://localhost:3000/articles")
+  const fetchArticles = () => {
     fetch("https://api.jsonbin.io/v3/b/655f73f712a5d376599d8e89", {
       method: "GET",
       headers: {
         "X-Master-Key": "$2a$10$H3/1OUh9KTdhHM6XmzYrTuE6LPD3zq/R/gg4FZssG3ZUCJv.J8zc6"
       }
     })
-      .then(r => r.json())
-      .then(articles => {
-        setArticles(articles);
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const articlesArray = data.record.articles;
+        setArticles(articlesArray);
         setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching articles:', error);
         setLoading(false);
       });
-  }
+  };
 
   useEffect(() => {
-    fetchedArticles();
+    fetchArticles();
   }, []);
 
-  function addArticles(newArticles) {
-    setArticles([...articles, newArticles])
-  }
+  const addArticle = (newArticle) => {
+    setArticles(prevArticles => [...prevArticles, newArticle]);
+  };
 
-  function deleteArticle(id) {
+  const deleteArticle = (id) => {
     fetch(`http://localhost:3000/articles/${id}`, {
       method: "DELETE",
     })
-      .then(resp => resp.json())
-      .then(() => {
-        const updatedArticles = articles.filter((article) => article.id !== id)
-        setArticles(updatedArticles)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
       })
-  }
+      .then(() => {
+        const updatedArticles = articles.filter(article => article.id !== id);
+        setArticles(updatedArticles);
+      })
+      .catch(error => {
+        console.error('Error deleting article:', error);
+      });
+  };
 
-  function handleUpdate(updatedArticle) {
-    const updatedArticles = articles.map((article) => {
-      if (article.id === updatedArticle.id) {
-        return updatedArticle;
-      } else {
-        return article;
-      }
-    });
+  const handleUpdate = (updatedArticle) => {
+    const updatedArticles = articles.map(article =>
+      article.id === updatedArticle.id ? updatedArticle : article
+    );
     setArticles(updatedArticles);
-  }
+    // Send a request to the server to persist the changes if needed
+  };
 
-  const arts = articles.map((article, index) =>
-    <ArticleItem key={index} name={article.name} title={article.title} image={article.image} description={article.description} content={article.content} id={article.id} author={article.author} deleteArticle={deleteArticle} onUpdateArticle={handleUpdate} />
-  );
+  const arts = articles.map(article => (
+    <ArticleItem
+      key={article.id}
+      name={article.name}
+      title={article.title}
+      image={article.image}
+      description={article.description}
+      content={article.content}
+      id={article.id}
+      author={article.author}
+      deleteArticle={deleteArticle}
+      onUpdateArticle={handleUpdate}
+    />
+  ));
 
   return (
     <div className="art">
       <div className='health'>
         <h1>Green Health Articles</h1>
-        <img src={health} alt="alternate-text" />
+        <img src={health} alt="Green Health" />
       </div>
       <div>
         {loading ? (
